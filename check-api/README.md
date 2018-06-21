0b803a827646cfa996035366a5b41a6287ae6413
Alexander Fefelov <alexanderfefelov@yandex.ru>
2018-06-21 13:58:50 +0300

    [check-api] Refactoring

---
+++ check-api/README.md
@@ -0,0 +1,3 @@
+# check-api
+
+This script check API availabity.
--- check-api/check-api.py
+++ check-api/check-api.py
@@ -22,6 +22,7 @@ def main():
     additional_passwords = config.get('application', 'additional_passwords').split(',')
     boxes_csv = config.get('application', 'boxes_csv')
     success_out = config.get('application', 'success_out')
+    unable_to_login_out = config.get('application', 'unable_to_login_out')
 
     logging.config.fileConfig('logging.conf')
 
@@ -33,47 +34,47 @@ def main():
             boxes.append({fieldnames[i]: r[fieldnames[i]] for i in range(len(fieldnames))})
     logging.info('%d box(es) will be checked', len(boxes))
 
-    success = []
-    unable_to_login = []
-    error = []
+    WRITE_BUFFER_SIZE = 0
+    with open(success_out, 'wb', WRITE_BUFFER_SIZE) as success_out_f, open(unable_to_login_out, 'wb', WRITE_BUFFER_SIZE) as unable_to_login_out_f:
+        success_out_writer = csv.writer(success_out_f)
+        unable_to_login_out_writer = csv.writer(unable_to_login_out_f)
 
-    for box in boxes:
-        host = box['host']
-        username = box['username']
-        password = box['password']
+        processed = 0
+        for box in boxes:
+            host = box['host']
+            username = box['username']
+            password = box['password']
 
-        usernames = [username] + additional_usernames
-        passwords = [password] + additional_passwords
+            usernames = [username] + additional_usernames
+            passwords = [password] + additional_passwords
 
-        logging.info('Processing %s', host)
-        for u in usernames:
-            for p in passwords:
-                try:
-                    api = connect(host = host, username = u, password = p, timeout = timeout)
-                    identity = api(cmd = '/system/identity/print')[0]['name']
-                    success.append((host, u, p, identity))
-                    api.close()
-                    break
-                except:
-                    exc_type, exc_value, exc_traceback = sys.exc_info()
-                    logging.error(exc_value)
-                    if (str(exc_value) != "cannot log in"):
-                        error.append((host, str(exc_value)))
+            logging.info('Processing %s', host)
+            for u in usernames:
+                for p in passwords:
+                    try:
+                        api = connect(host = host, username = u, password = p, timeout = timeout)
+                        identity = api(cmd = '/system/identity/print')[0]['name']
+                        success_out_writer.writerow((host, u, p, identity))
+                        logging.info("Success")
+                        api.close()
                         break
+                    except:
+                        exc_type, exc_value, exc_traceback = sys.exc_info()
+                        logging.error(exc_value)
+                        if (str(exc_value) != "cannot log in"):
+                            break
+                else:
+                    continue
+                break
             else:
-                continue
-            break
-        else:
-            unable_to_login.append(host)
-        logging.info('%s done', host)
-
-    with open(success_out, 'wb') as f:
-        writer = csv.writer(f)
-        for r in success:
-            writer.writerow(r)
+                unable_to_login_out_writer.writerow([host])
+            logging.info('%s done', host)
+            processed += 1
+            if (processed % 50 == 0):
+                logging.info('%d boxes processed', processed)
+        logging.info('Total %d boxes processed', processed)
 
 
-    print(success)
 
 if __name__ == '__main__':
     main()
